a29f4f999c6e688900f14ec7e0173a5258118a57
Abdul Fatah <fatahhikami@gmail.com>
2020-02-26 16:37:16 +0700

    Add File Validation

--- modules/excel.php
+++ modules/excel.php
@@ -2,31 +2,36 @@
 
 require '../vendor/autoload.php';
 if(isset($_POST["submit"])) {
-    $data = $_FILES['file']['tmp_name'];
-    $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($data);
-    $worksheet = $spreadsheet->getActiveSheet();
-    $highestRow = $worksheet->getHighestRow();
-    $highestColumn = $worksheet->getHighestColumn();
-    $getActiveCell = $worksheet->getActiveCell();
-    $highestColumnIndex = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($highestColumn);
+    $file = $_FILES['file']['tmp_name'];
+    $filename = $_FILES['video_file']['name'];
     
-    $rows = [];
-    for($row=1;$row<$highestRow;$row++){ 
-        $data = [];
-        for ($col = 1; $col <= $highestColumnIndex;$col++) {
-            if (!empty($row)) {
-                $key = $worksheet->getCellByColumnAndRow($col, 1)->getValue();
-                $value = $worksheet->getCellByColumnAndRow($col, $row+1)->getValue();
+    $exts = array('xls', 'xlsx'); 
+    if(in_array(end(explode('.', $filename)), $exts)){
+        $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($file);
+        $worksheet = $spreadsheet->getActiveSheet();
+        $highestRow = $worksheet->getHighestRow();
+        $highestColumn = $worksheet->getHighestColumn();
+        $getActiveCell = $worksheet->getActiveCell();
+        $highestColumnIndex = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($highestColumn);
+        
+        $rows = [];
+        for($row=1;$row<$highestRow;$row++){ 
+            $data = [];
+            for ($col = 1; $col <= $highestColumnIndex;$col++) {
+                if (!empty($row)) {
+                    $key = $worksheet->getCellByColumnAndRow($col, 1)->getValue();
+                    $value = $worksheet->getCellByColumnAndRow($col, $row+1)->getValue();
 
-                if ($key != null) {
-                    $data[$key] = $value;
+                    if ($key != null) {
+                        $data[$key] = $value;
+                    }
                 }
             }
+            if (!empty($data)) {
+                array_push($rows,$data);
+            }
         }
-        if (!empty($data)) {
-            array_push($rows,$data);
-        }
-    }
 
-    echo json_encode($rows);
+        echo json_encode($rows);
+    }
 }
